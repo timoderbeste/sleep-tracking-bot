c7e888c3fdd12c9df29f9941f7dc9ab2c057c8a0
Jingya Xun <jxun@sandiego.edu>
2019-06-24 11:57:00 -0500

    smsbot_flask initial upload

---
+++ smsbot_flask/automated_survey_flask/__init__.py
@@ -0,0 +1,27 @@
+from automated_survey_flask.config import config_env_files
+from flask import Flask
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+app = Flask(__name__)
+
+
+def prepare_app(environment='development', p_db=db):
+    app.config.from_object(config_env_files[environment])
+    p_db.init_app(app)
+    # load views by importing them
+    from . import views
+    return app
+
+
+def save_and_commit(item):
+    db.session.add(item)
+    db.session.commit()
+
+def delete_and_commit(item):
+    db.session.delete(item)
+    db.session.commit()
+
+db.save = save_and_commit
+db.delete = delete_and_commit
