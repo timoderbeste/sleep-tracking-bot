a5d157f33c3ce4fd156506d4383348bf34e0f3a4
timoderbeste <ntwang1994@gmail.com>
2019-06-29 20:32:57 -0500

    Adding adminfrontend. Code taken from https://github.com/akveo/ngx-admin

--- emile/emile/__init__.py
+++ emile/emile/__init__.py
@@ -1,14 +1,19 @@
 from emile.config import config_env_files
 from flask import Flask
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 app = Flask(__name__)
+admin = Admin(app, name='emile admin', template_mode='bootstrap3')
 
 
 def prepare_app(environment='development', p_db=db):
-    app.config.from_object(config_env_files[environment])
     p_db.init_app(app)
+    app.config.from_object(config_env_files[environment])
+    app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+    admin.add_view(ModelView(User, p_db.session))
     # load views by importing them
     from . import views
     return app
