80da62c49f11613e7544bf3561734008f44a38a3
timoderbeste <ntwang1994@gmail.com>
2019-06-29 20:52:09 -0500

    Adding a playground page to test different components and try to connect with backend apis

--- admin-frontend/src/app/pages/playground/playground.component.ts
+++ admin-frontend/src/app/pages/playground/playground.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ngx-playground',
   templateUrl: './playground.component.html',
-  styleUrls: ['./playground.component.scss']
 })
 export class PlaygroundComponent implements OnInit {
 
---
+++ admin-frontend/src/app/pages/playground/playground.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { PlaygroundComponent } from './playground.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [
+    PlaygroundComponent,
+  ],
+  providers: [
+  ],
+})
+export class PlaygroundModule { }
