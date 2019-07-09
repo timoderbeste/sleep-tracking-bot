09d9ac8d631cb584669e27d9e036c9f3aed6f04a
CaesarTY <tengyun427@gmail>
2019-07-09 17:46:22 -0500

    add more api

---
+++ database_server/botDB/main/plan_views.py
@@ -0,0 +1,56 @@
+from flask import request, session, redirect, url_for, current_app, jsonify
+from .. import db
+from ..models import Datum, User, Plan, Plan
+from . import main
+
+
+
+@main.route('/plans', methods=['GET'])
+def get_plans():
+    allPlan = Plan.query.all()
+    data = list()
+    for p in allPlan:
+        data.append(p.to_dict())
+    return jsonify({'plans': data})
+
+
+@main.route('/plan/id/<int:plan_id>', methods=['GET'])
+def get_plan_byID(plan_id: int):
+    targetPlan = Plan.query.get_or_404(plan_id)
+    return jsonify({'plan': targetPlan.to_dict()}), 201
+
+
+@main.route('/plan', methods=['POST'])
+def create_plan():
+    if not request.json:
+        abort(400)
+
+    newPlan = Plan(weeklyBedtime = request.json['weeklyBedtime'], weeklyFrequency = request.json['weeklyFrequency'])
+
+    db.session.add(newPlan)
+    db.session.commit()
+    return jsonify({'plan': newPlan.to_dict()}), 201
+
+
+@main.route('/plan/<int:plan_id>', methods=['PUT'])
+def update_plan(plan_id: int):
+    if not request.json:
+        abort(400)
+
+    targetPlan = Plan.query.get_or_404(plan_id)
+    targetPlan.weeklyBedtime = request.json['weeklyBedtime']
+    targetPlan.weeklyFrequency = request.json['weeklyFrequency']
+    db.session.add(targetPlan)
+    db.session.commit()
+    return jsonify({'plan': targetPlan.to_dict()}), 201
+ 
+
+
+@main.route('/plan/<int:plan_id>', methods=['DELETE'])
+def delete_plan(plan_id: int):
+    targetPlan = Plan.query.get_or_404(plan_id)
+    targetDict = targetPlan.to_dict()
+    db.session.delete(targetPlan)
+    db.session.commit()
+
+    return jsonify({'plan': targetDict}), 201
---
+++ database_server/botDB/main/update_user.py
@@ -0,0 +1,98 @@
+from flask import request, session, redirect, url_for, current_app, jsonify
+from .. import db
+from ..models import Datum, User, Record, Plan
+from . import main
+
+
+
+
+@main.route('/user/<int:user_id>/update', methods=['PUT'])
+def update_user_info(user_id: int):
+    if not request.json:
+        abort(400)
+
+    targetUser = User.query.get_or_404(user_id)
+    updateDict = dict(request.json)
+    if 'currentState' in updateDict:
+        targetUser.currentState = updateDict['currentState']
+    if  'idealBedtime'in updateDict:
+        targetUser.idealBedtime = updateDict['idealBedtime']
+    if  'currentBedtime'in updateDict:
+        targetUser.currentBedtime = updateDict['currentBedtime']
+    if  'timezone'in updateDict:
+        targetUser.timezone = updateDict['timezone']
+    db.session.add(targetUser)
+    db.session.commit()
+    return jsonify({'user': targetUser.to_dict()}), 201
+
+
+@main.route('/user/<int:user_id>/hit++', methods=['PUT'])
+def update_hit_plus(user_id: int):
+
+    targetUser = User.query.get_or_404(user_id)
+    targetUser.weeklyHit += 1
+    db.session.add(targetUser)
+    db.session.commit()
+    return jsonify({'user': targetUser.to_dict()}), 201
+
+@main.route('/user/<int:user_id>/hit--', methods=['PUT'])
+def update_hit_minus(user_id: int):
+
+    targetUser = User.query.get_or_404(user_id)
+    targetUser.weeklyHit -= 1
+    db.session.add(targetUser)
+    db.session.commit()
+    return jsonify({'user': targetUser.to_dict()}), 201
+
+@main.route('/user/<int:user_id>/miss++', methods=['PUT'])
+def update_miss_plus(user_id: int):
+   
+    targetUser = User.query.get_or_404(user_id)
+    targetUser.weeklyMiss += 1
+    db.session.add(targetUser)
+    db.session.commit()
+    return jsonify({'user': targetUser.to_dict()}), 201
+
+@main.route('/user/<int:user_id>/miss--', methods=['PUT'])
+def update_miss_minus(user_id: int):
+    
+    targetUser = User.query.get_or_404(user_id)
+    targetUser.weeklyMiss -= 1
+    db.session.add(targetUser)
+    db.session.commit()
+    return jsonify({'user': targetUser.to_dict()}), 201
+
+@main.route('/user/<int:user_id>/missreset', methods=['PUT'])
+def update_hit_reset(user_id: int):
+    
+    targetUser = User.query.get_or_404(user_id)
+    targetUser.weeklyMiss = 0
+    db.session.add(targetUser)
+    db.session.commit()
+    return jsonify({'user': targetUser.to_dict()}), 201
+
+@main.route('/user/<int:user_id>/hitreset', methods=['PUT'])
+def update_miss_reset(user_id: int):
+    
+    targetUser = User.query.get_or_404(user_id)
+    targetUser.weeklyHit = 0
+    db.session.add(targetUser)
+    db.session.commit()
+    return jsonify({'user': targetUser.to_dict()}), 201
+
+@main.route('/user/<int:user_id>/changeplan', methods=['PUT'])
+def update_changeplan(user_id: int):
+    if not request.json:
+        abort(400)
+
+    updateWeeklyBedtime = request.json['weeklyBedtime']
+    updateWeeklyFrequency = request.json['weeklyFrequency']
+    updatePlan = Plan(weeklyFrequency = updateWeeklyFrequency, weeklyBedtime = updateWeeklyBedtime)
+    db.session.add(updatePlan)
+    db.session.commit()
+    
+    targetUser = Plan.query.get_or_404(user_id)
+    targetUser.weeklyPlanId = updatePlan.id
+    db.session.add(targetUser)
+    db.session.commit()
+    return jsonify({'user': targetUser.to_dict()}), 201
