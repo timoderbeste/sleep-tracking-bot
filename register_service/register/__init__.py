07cd7737f54c15b08d2dc78fed8f2b02d16ce137
CaesarTY <tengyun427@gmail>
2019-07-11 22:01:12 -0500

    debug

--- register_service/register/__init__.py
+++ register_service/register/__init__.py
@@ -18,16 +18,6 @@ class RegisterForm(FlaskForm):
     submit = SubmitField('Submit')
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
-
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = RegisterForm()
