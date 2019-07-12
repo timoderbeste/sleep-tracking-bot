aa1fc0e9c952374cd11869d67df1b11a1d50a304
CaesarTY <tengyun427@gmail>
2019-07-11 22:40:17 -0500

    add servers connect

--- register_service/register/__init__.py
+++ register_service/register/__init__.py
@@ -4,6 +4,7 @@ from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+import requests
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -11,12 +12,27 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+DB_URL = 'http://127.0.0.1:5000/'
+SMS_URL = 'http://69f52c2b.ngrok.io/'
 
 class RegisterForm(FlaskForm):
-    userName = StringField('Please enter your Username', validators=[DataRequired()])
+    userName = StringField('Please enter your username', validators=[DataRequired()])
     phone = StringField('Please enter your phone number', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+
+def add_user(userName, phone):
+   URL = DB_URL + 'user/create'
+   data = dict()
+   data["userName"] = userName
+   data["phone"] = phone
+   requests.post(url = URL, json = data)
+
+def notify_sms(phone):
+   URL = SMS_URL + 'newuser'
+   data = dict()
+   data["phone"] = phone
+   requests.post(url = URL, json = data)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -24,6 +40,7 @@ def index():
     if form.validate_on_submit():
         session['userName'] = form.userName.data
         session['phone'] = form.phone.data
+        add_user(form.userName.data, form.phone.data)
+        notify_sms(form.phone.data)
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('userName'))
-
